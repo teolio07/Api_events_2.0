456eacf46fe2b8654a08f0b74b42e9259091b53b
Jheison <ruateo555@gmail.com>
2022-12-12 22:59:57 -0500

    Falta agregar jwt

--- database/database.js
+++ database/database.js
@@ -1,19 +1,19 @@
 import Sequelize from 'sequelize';
 
 
-/*
 const db_name = 'b2lq1uoz9pke3mi4m9sw';
 const username = 'u93ovu0rem0kex58';
 const password = 'Qm92ZSBTRasDTFvHna7B';
 const host = 'b2lq1uoz9pke3mi4m9sw-mysql.services.clever-cloud.com';
 const dialect = 'mysql'
-*/
+
+/*
 const db_name = 'DB_EVENT';
 const username = 'root';
 const password = 'mono19teo';
 const host = 'localhost';
 const dialect = 'mysql'
-
+*/
 
 export const sequelize = new Sequelize(db_name,username,password,{host: host,dialect: dialect})
 
